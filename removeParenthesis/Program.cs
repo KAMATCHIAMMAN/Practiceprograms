2aebd450d0e9663f59bf8425007def2d6981cef8
KAMATCHIAMMAN <113223223+KAMATCHIAMMAN@users.noreply.github.com>
2023-04-04 22:35:33 +0530

    Add files via upload

---
+++ removeParenthesis/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace removeParenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Kama(kama)tchi";
+            int a = str.IndexOf('(');
+            int b = str.IndexOf(')');
+            string s2 = "";
+            s2=str.Remove(a, Math.Abs((b+1)-a));
+            Console.WriteLine(s2);
+        }
+    }
+}
