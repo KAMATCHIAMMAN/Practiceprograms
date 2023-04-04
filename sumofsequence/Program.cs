6a4d538d362f64a0f0bda4211734c25692424761
KAMATCHIAMMAN <113223223+KAMATCHIAMMAN@users.noreply.github.com>
2023-04-04 22:40:38 +0530

    Add files via upload

---
+++ sumofsequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace sumofsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start, last, increment;
+            Console.WriteLine("Enter start,end,increment");
+            start=Convert.ToInt32(Console.ReadLine());
+            last=Convert.ToInt32(Console.ReadLine());
+            increment=Convert.ToInt32(Console.ReadLine());
+            int s = 0;
+            if (start>last)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                for (int i = start; i<=last; i=i+increment)
+                {
+                    s=s+i;
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine(s);
+
+            }
+        }
+    }
+}
