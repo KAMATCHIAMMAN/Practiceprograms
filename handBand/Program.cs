5253365cda13c70e1a3a7accdf98b42ea94f4eec
KAMATCHIAMMAN <113223223+KAMATCHIAMMAN@users.noreply.github.com>
2023-04-05 22:33:20 +0530

    Add files via upload

---
+++ handBand/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+namespace handBand
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string");
+            string str = Console.ReadLine(); ;
+            string resultString = "";
+            char first = str[0];
+            char last = str[(str.Length)-1];
+            Console.WriteLine(first);
+            Console.WriteLine(last);
+            if (first.Equals(last))
+            {
+                resultString=resultString+str;
+                resultString=resultString+str;
+                Console.WriteLine(char.ToUpper(resultString[0])+resultString.Substring(1));
+            }
+            else
+            {
+                resultString=resultString+str;
+                Console.WriteLine("The"+char.ToUpper(resultString[0])+resultString.Substring(1));
+
+            }
+        }
+    }
+}
