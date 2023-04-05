3fea90b601ef1e58506856cb5f4df777b3d601e0
KAMATCHIAMMAN <113223223+KAMATCHIAMMAN@users.noreply.github.com>
2023-04-05 22:35:05 +0530

    Add files via upload

---
+++ hidenumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+namespace hidenumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string:");
+            string string1 = Console.ReadLine();
+            string string2 = "";
+            string string3 = "";
+            for (int i = 0; i<((string1.Length)-4); i++)
+            {
+                string2=string2+"#";
+            }
+            string3=string1.Substring((string1.Length)-4);
+            Console.WriteLine(string2+string3);
+        }
+    }
+}
