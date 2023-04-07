5c05cf7d812e922f55323f9c6b72b71b5db0e517
KAMATCHIAMMAN <113223223+KAMATCHIAMMAN@users.noreply.github.com>
2023-04-07 13:41:28 +0530

    Add files via upload

---
+++ MultipleInheritenceexample/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Demo
+{
+    class Son : Father
+    {
+        public void DisplayTwo()
+        {
+            Console.WriteLine("My name is kamatchi ");
+        }
+        static void Main(string[] args)
+        {
+            Son s = new Son();
+            s.Display();
+            s.DisplayOne();
+            s.DisplayTwo();
+            Console.Read();
+        }
+    }
+    class Grandfather
+    {
+        public void Display()
+        {
+            Console.WriteLine("I am black color");
+        }
+    }
+    class Father : Grandfather
+    {
+        public void DisplayOne()
+        {
+            Console.WriteLine("My height is 156");
+        }
+    }
+}
