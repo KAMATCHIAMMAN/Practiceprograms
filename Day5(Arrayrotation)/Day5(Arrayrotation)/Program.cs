a0e317c0d67f65bdc44b9e26cea6a53d37e8a3c4
KAMATCHIAMMAN <113223223+KAMATCHIAMMAN@users.noreply.github.com>
2023-04-09 19:50:46 +0530

    Add files via upload

---
+++ Day5(Arrayrotation)/Day5(Arrayrotation)/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+public class Loop
+{
+    static void showFirst(int[] newarr)
+    {
+        Console.WriteLine("The first item of the array is:"+newarr[0]);
+    }
+    static void popOut(int[] newarr)
+    {
+        var ele = newarr.Skip(1);
+        Console.WriteLine("New Array after removing the first element...");
+        foreach (var res in ele)
+        {
+            Console.WriteLine(res);
+        }
+    }
+    public static void Main()
+    {
+        Console.WriteLine("Enter the no.of elemeents in the list:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[num];
+        for (int i = 0; i<num; i++)
+        {
+            arr[i]=int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Array before rotation");
+        foreach (int i in arr)
+        {
+            Console.WriteLine(i);
+        }
+        int[] newarr = new int[num];
+        for (int i = 0; i<num-1; i++)
+        {
+            newarr[i]=arr[i+1];
+        }
+        newarr[num-1]=arr[0];
+        Console.WriteLine("Array after the rotation");
+        foreach (int i in newarr)
+        {
+            Console.WriteLine(i);
+        }
+        showFirst(newarr);
+        popOut(newarr);
+    }
+}
