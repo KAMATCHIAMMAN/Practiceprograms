d0bdb93869ae8c4ce3358ecf7532cb813ba2f38e
KAMATCHIAMMAN <113223223+KAMATCHIAMMAN@users.noreply.github.com>
2023-04-10 18:06:09 +0530

    Add files via upload

---
+++ EvenOddIndex/EvenOddIndex/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EvenOddIndex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the String :");
+            string str = Console.ReadLine();
+            string firstpart = "";
+            string secondpart = "";
+            for(int i=0;i<str.Length;i++)
+            {
+                if(i%2==0)
+                {
+                    firstpart=firstpart+str[i];
+                }
+                else
+                {
+                    secondpart=secondpart+str[i];
+                }
+            }
+            Console.WriteLine(firstpart+" "+secondpart);
+
+        }
+    }
+}
