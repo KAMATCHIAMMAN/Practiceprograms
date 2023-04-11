17a39eb1d2a040001785edf414bd1854a5c8d631
KAMATCHIAMMAN <113223223+KAMATCHIAMMAN@users.noreply.github.com>
2023-04-11 18:44:54 +0530

    Add files via upload

---
+++ GenericMethodExample/GenericMethodExample/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericMethodExample
+{
+    using System;
+    class Add<T1, T2>
+    {
+        public void added(T1 dataone, T2 datatwo)
+        {
+            Console.WriteLine("Addedd value is: "+dataone+datatwo);
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Add<string, string> obj = new Add<string, string>();
+            obj.added("kama", "sri");
+            Add<int, int> objone = new Add<int, int>();
+            objone.added(200, 100);
+        }
+    }
+}
