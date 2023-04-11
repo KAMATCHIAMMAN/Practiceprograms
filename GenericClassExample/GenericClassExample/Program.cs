2841242b26a1cd37d97400700e133512579db099
KAMATCHIAMMAN <113223223+KAMATCHIAMMAN@users.noreply.github.com>
2023-04-11 18:46:09 +0530

    Add files via upload

---
+++ GenericClassExample/GenericClassExample/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace GenericClassExample
+{
+    using System;
+    public class Hotel<T1, T2>
+    {
+        private T1 food;
+        private T2 amount;
+        public Hotel(T1 food, T2 amount)
+        {
+            this.food=food;
+            this.amount=amount;
+        }
+        public void Menu()
+        {
+            Console.WriteLine("Type of the food is "+this.food);
+            Console.WriteLine("Amount of the food is "+this.amount);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Hotel<string, int> foodvarity = new Hotel<string, int>("Dosa", 60);
+            foodvarity.Menu();
+        }
+    }
+}
